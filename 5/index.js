ddc6f17a82f4f5f11290d99b344409cc61988114
Stanislav <stanislavasilevich@gmail.com>
2025-03-04 19:22:36 +0300

    completed new task

---
+++ 5/index.js
@@ -0,0 +1,10 @@
+/*
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+* */
+
+const booleanToString = String;
+
+console.log(booleanToString(true)); // "true", 'When we pass in true, we want the string "true" as output';
+console.log(booleanToString(false)); // "false", ;
